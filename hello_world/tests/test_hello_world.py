6b3289eaffe82cb0d98262bb806199cf50082a61
timur <timurkupaev@gmail.com>
2024-12-01 23:49:55 +0300

    изменил тесты

--- hello_world/tests/test_hello_world.py
+++ hello_world/tests/test_hello_world.py
@@ -1,6 +1,11 @@
+import sys
+import os
+
+# Добавляем путь к папке src в sys.path, чтобы Python мог найти модули
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+# Теперь Python может найти модуль hello_world
 from hello_world.src.hello_world import hello
-
-
 
 def test_hello():
     assert hello() == "Hello, World!"
